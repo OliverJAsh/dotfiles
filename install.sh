fa122934b931c1ffba14a5030ea4eef50d3f4d95
Oliver Joseph Ash <oliverjash@gmail.com>
2023-11-16 09:43:56 +0000

    Fix command

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ chsh -s /run/current-system/sw/bin/fish
 
 ln -s "$PWD"/config/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s "$PWD"/config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s "$PWD"/config/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets/
+ln -s "$PWD"/config/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets
 
 rm -rf /opt/homebrew/etc/nginx/nginx.conf
 ln -s "$PWD"/config/nginx.conf /opt/homebrew/etc/nginx/nginx.conf
