68697fc5dd131afce758951ce40a1e7dfbfa6023
Oliver Joseph Ash <oliverjash@gmail.com>
2025-09-14 09:39:45 +0100

    Personal: remove 1Password for Safari

--- hosts/personal/darwin.nix
+++ hosts/personal/darwin.nix
@@ -2,9 +2,6 @@
   imports = [ ../common/darwin.nix ];
 
   homebrew = {
-    masApps = {
-      "1Password for Safari" = 1569813296;
-    };
     casks = [
       "plex-media-server"
       "transmission"
