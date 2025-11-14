6cdd441a2ae396207cfffff5d5cb23033e8cbd61
Oliver Joseph Ash <oliverjash@gmail.com>
2025-11-14 12:49:32 +0000

    jjui 0.9.8 (https://github.com/NixOS/nixpkgs/pull/469978)

--- flake.nix
+++ flake.nix
@@ -20,12 +20,28 @@
       nix-vscode-extensions,
       ...
     }:
+    let
+      jjuiOverlay = final: prev: {
+        jjui = prev.jjui.overrideAttrs (_old: {
+          version = "0.9.8";
+          src = prev.fetchFromGitHub {
+            owner = "idursun";
+            repo = "jjui";
+            tag = "v0.9.8";
+            hash = "sha256-YEEcSaIm21IUp7EFdYvDG2h55YIqzghYdGxdXmZnp9I=";
+          };
+          vendorHash = "sha256-2TlJJY/eM6yYFOdq8CcH9l2lFHJmFrihuGwLS7jMwJ0=";
+        });
+      };
+    in
     {
       darwinConfigurations."Olivers-MacBook-Pro" = darwin.lib.darwinSystem {
         modules = [
           ./hosts/work/darwin.nix
           home-manager.darwinModules.home-manager
           {
+            nixpkgs.overlays = [ jjuiOverlay ];
+
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.oliver = import ./hosts/work/home.nix;
@@ -39,6 +55,8 @@
           ./hosts/personal/darwin.nix
           home-manager.darwinModules.home-manager
           {
+            nixpkgs.overlays = [ jjuiOverlay ];
+
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.oliver = import ./hosts/personal/home.nix;
