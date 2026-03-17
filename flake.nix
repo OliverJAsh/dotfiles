27050293b50d9507d98806eba5e25f53502a4b57
Oliver Joseph Ash <oliverjash@gmail.com>
2026-03-17 00:32:55 +0000

    jjui 0.10.2

--- flake.nix
+++ flake.nix
@@ -38,12 +38,12 @@
               })
               (final: prev: {
                 jjui = prev.jjui.overrideAttrs (old: rec {
-                  version = "0.10.1";
+                  version = "0.10.2";
                   src = prev.fetchFromGitHub {
                     owner = "idursun";
                     repo = "jjui";
                     tag = "v${version}";
-                    hash = "sha256-o648hMSoEa21GqK4VGSa4hf5KP5FVu80Ea5NB2QwII0=";
+                    hash = "sha256-VTaOd5LSBxo6EhTyjoyAqX+wTEcm88qIgUCcd+TRYY4=";
                   };
                   vendorHash = "sha256-GDYgZI6X7UwnyKXOJVmqXXtm4ulA10uuX5MeqKVTheA=";
                 });
